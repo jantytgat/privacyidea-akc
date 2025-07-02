134eb98fcd14c6d758f7303c9e04139e0083d7f8
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-07-02 22:41:27 +0200

    Copy main.go
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

---
+++ main.go
@@ -0,0 +1,16 @@
+package privacyidea_akc
+
+import "fmt"
+
+const (
+	PI_APPLICATION         = "ssh"
+	PI_DEFAULT_CONFIG_PATH = "/etc/privacyidea/authorizedkeyscommand"
+)
+
+var (
+	PI_DEFAULT_HEADERS = []string{"Content-Type: application/json", "Accept: application/json"}
+)
+
+func main() {
+	fmt.Println("piakc v0.1.0")
+}
